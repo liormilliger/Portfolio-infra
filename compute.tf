db32b52096fdf51ba80172018b8830d27a73a737
Lior <liormdevops@gmail.com>
2024-01-14 18:07:11 +0200

    Jenkins instance hashed

--- compute.tf
+++ compute.tf
@@ -3,23 +3,23 @@ resource "aws_key_pair" "liorm_key" {
   public_key = file("~/.ssh/liorm-portfolio-key.pub")
 }
 
-resource "aws_instance" "my-tf-machine" {
-  ami                    = "ami-01c115fd0ea0ea9a2"
-  instance_type          = "t3a.large"
-  key_name               = aws_key_pair.liorm_key.id
-  availability_zone      = "us-east-1a"
-  subnet_id              = aws_subnet.us-east-sub1.id
-  vpc_security_group_ids = [aws_security_group.liorm-portfolio-SG.id]
+# resource "aws_instance" "my-tf-machine" {
+#   ami                    = "ami-01c115fd0ea0ea9a2"
+#   instance_type          = "t3a.large"
+#   key_name               = aws_key_pair.liorm_key.id
+#   availability_zone      = "us-east-1a"
+#   subnet_id              = aws_subnet.us-east-sub1.id
+#   vpc_security_group_ids = [aws_security_group.liorm-portfolio-SG.id]
   
-  # ebs_block_device {
-  #   device_name = "Jenkins-Volume"
-  #   snapshot_id = "snap-091ad9e7cd4cb0736"
-  # }
+#   # ebs_block_device {
+#   #   device_name = "Jenkins-Volume"
+#   #   snapshot_id = "snap-091ad9e7cd4cb0736"
+#   # }
 
-  tags = {
-    Name = "Jenkins-Server-TF"
-  }
-}
+#   tags = {
+#     Name = "Jenkins-Server-TF"
+#   }
+# }
 
 resource "kubernetes_namespace" "argocd" {
   metadata {
