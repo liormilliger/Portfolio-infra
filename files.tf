dc0302009e6ddd628b03db54ffe2f513be51f35a
Lior <liormdevops@gmail.com>
2024-02-03 21:14:31 +0200

    files.tf file changed

--- files.tf
+++ files.tf
@@ -2,11 +2,10 @@ resource "kubectl_manifest" "storage_class_csi" {
   yaml_body = file("${path.module}/files/storage-class-csi.yaml")
 }
 
-resource "kubectl_manifest" "service_monitor" {
-  yaml_body  = file("${path.module}/files/prometheus-service-monitor.yaml")
-  depends_on = [module.eks.argocd_helm]
-}
-
+# resource "kubectl_manifest" "service_monitor" {
+#   yaml_body  = file("${path.module}/files/prometheus-service-monitor.yaml")
+#   depends_on = [module.eks.argocd_helm]
+# }
 
 resource "kubectl_manifest" "fluentd_configmap" {
   yaml_body  = file("${path.module}/files/fluentd-cm-GPT.yaml")
