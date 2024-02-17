7770a58ed698da5f745c6cecf0bb1d89be97106a
Lior <liormdevops@gmail.com>
2024-02-17 16:24:51 +0200

    app-of-apps up from tf

--- files.tf
+++ files.tf
@@ -2,18 +2,8 @@ resource "kubectl_manifest" "storage_class_csi" {
   yaml_body = file("${path.module}/files/storage-class-csi.yaml")
 }
 
-# resource "kubectl_manifest" "service_monitor" {
-#   yaml_body  = file("${path.module}/files/prometheus-service-monitor.yaml")
-#   depends_on = [module.eks.argocd_helm]
-# }
-
-# resource "kubectl_manifest" "fluentd_configmap" {
-#   yaml_body  = file("${path.module}/files/fluentd-cm.yaml")
-#   depends_on = [module.eks.fluentd_ns]
-# }
-
-# resource "kubectl_manifest" "argo_cd_apps" {
-#   yaml_body  = file("${path.module}/files/app-of-apps.yaml")
-#   depends_on = [ module.eks.argocd_helm ]
-# }
+resource "kubectl_manifest" "argo_cd_apps" {
+  yaml_body  = file("${path.module}/files/app-of-apps.yaml")
+  depends_on = [ module.eks.argocd_helm ]
+}
 
